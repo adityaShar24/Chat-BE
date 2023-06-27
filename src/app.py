c18ca07ac7afd6e0d1eb066e731834c9739d4800
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-06-27 08:04:12 +0000

    Added Files

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_jwt_extended import JWTManager
 from routes.user_router import auth_bp
+from routes.room_router import room_bp
 
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.config['SECRET_KEY'] = 'my_secret_key'
 jwt = JWTManager(app)
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(room_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
