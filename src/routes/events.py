8356511db61652def27040f85930125802ebcc3d
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-01 08:24:25 +0000

    Modified Files

--- src/routes/events.py
+++ src/routes/events.py
@@ -11,9 +11,3 @@ def handle_connect():
 def handle_userjoin(payload):
     print(f"User {payload['username']} joined!")
     join_room({payload['roomID']})
-    
-    
-@socketio.on("create_room")
-def handle_room(data):
-    join_room(data['roomID'])
-    print(f"User {data['username']} created a room : {data['roomID']}")
